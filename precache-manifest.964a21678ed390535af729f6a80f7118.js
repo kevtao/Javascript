73cc5cced987960654bb122c6a48ac35f25a91cb
wtao <wtao@redfastlabs.com>
2020-06-14 11:34:55 -0700

    Updates

---
+++ precache-manifest.964a21678ed390535af729f6a80f7118.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9ef86caee2c45fbbcd93c535e5eac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b86cdc9126b6863ec2a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "811757cd5dabab623800",
+    "url": "/static/js/2.96b49174.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.96b49174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b86cdc9126b6863ec2a",
+    "url": "/static/js/main.b14a0932.chunk.js"
+  },
+  {
+    "revision": "141b7d42e3227beb65c8",
+    "url": "/static/js/runtime-main.94f71fc2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
